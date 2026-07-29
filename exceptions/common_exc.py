3d1c5ab3c9b1e5fbb45174c8e9f4b234ddc3703c
Krupa Sodagar <krupavsodagar@gmail.com>
2026-07-29 17:10:52 +0530

    Update common_exc.py

--- exceptions/common_exc.py
+++ exceptions/common_exc.py
@@ -1,1 +1,12 @@
+examples = [
+    "ValueError",
+    "TypeError",
+    "IndexError",
+    "KeyError",
+    "ZeroDivisionError",
+    "FileNotFoundError",
+]
 
+for item in examples:
+    print(item)
+  
