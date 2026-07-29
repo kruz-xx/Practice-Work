263662555fdf80ff47ec275d040fe1942a76d6b2
Krupa Sodagar <krupavsodagar@gmail.com>
2026-07-29 17:11:46 +0530

    Update num_val.py

--- input_validation/num_val.py
+++ input_validation/num_val.py
@@ -1,1 +1,9 @@
+while True:
+    user_input = input("Enter a number: ")
 
+    try:
+        number = float(user_input)
+        print("Valid number:", number)
+        break
+    except ValueError:
+        print("Invalid input. Try again.")
